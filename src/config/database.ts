3291943c814c237e5d3896d8f2d83410f7269eea
Yeabtsega Mesfin <yeabtsegamesfin01@gmail.com>
2025-09-14 16:51:59 +0300

    Update database.ts
    Edit MONGODB_URI

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/bingo2025';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export const connectDatabase = async (): Promise<void> => {
   try {
